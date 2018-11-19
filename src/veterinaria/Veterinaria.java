809892104b2852658644e56d7ae3be073caaad88
AkiraReiDeveloper <45138556+AkiraReiDeveloper@users.noreply.github.com>
2018-11-18 18:23:47 -0700

    Create Veterinaria.java

---
+++ src/veterinaria/Veterinaria.java
@@ -0,0 +1,16 @@
+ /**
+ * @(#)CuadrosLista.java
+ *
+ *
+ * @author RGVM & CBA
+ * @version 1.00 2018/11/28
+ */
+package veterinaria;
+
+public class Veterinaria {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
